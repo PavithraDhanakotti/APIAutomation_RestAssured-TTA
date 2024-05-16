2e7ae926ab0d480430ac8541b0f0851c1fcdc99f
Pavithra Dhanakotti <dhanakotipavithra26@gmail.com>
2024-05-16 19:08:30 +0530

    Updating the code:16-05-24

---
+++ src/test/java/com/restassured/MyPractice/Lab0012_Deserialization.java
@@ -0,0 +1,21 @@
+package com.restassured.MyPractice;
+
+import com.google.gson.Gson;
+import org.testng.annotations.Test;
+
+public class Lab0012_Deserialization {
+
+    @Test
+    public void deserial01(){
+
+        String jsonString = "{\r\n" + "  \"firstName\" : \"Pramod\",\r\n" + "  \"lastName\" : \"Dutta\",\r\n"
+                + "  \"gender\" : \"M\",\r\n" + "  \"age\" : 31,\r\n" + "  \"salary\" : 3422.33,\r\n"
+                + "  \"married\" : false\r\n" + "}";
+
+        Gson gson = new Gson();
+        com.restassured.ClassNotes.misc.gson.example.Employee employeeObject = gson.fromJson(jsonString,com.restassured.ClassNotes.misc.gson.example.Employee.class);
+
+        String firstName = employeeObject.getFirstName();
+        String lastName = employeeObject.getLastName();
+    }
+}
